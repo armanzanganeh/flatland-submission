b02bcfe6553c512d7a1fc21fa853b0313571fdc0
Arman_Zanganeh <armanzanganeh7@gmail.com>
2026-05-31 10:55:13 +0200

    use DeadlockAvoidanceHeuristics

--- submission/my_observation_builder.py
+++ submission/my_observation_builder.py
@@ -1,11 +1,2 @@
-from flatland.core.env_observation_builder import ObservationBuilder
-
-class MyObservationBuilder(ObservationBuilder):
-    def get(self, handle=0):
-        return self.env
-
-    def reset(self):
-        pass
-
-    def set_env(self, env):
-        self.env = env
+from flatland_baselines.deadlock_avoidance_heuristic.observation.full_env_observation import FullEnvObservation
+MyObservationBuilder = FullEnvObservation
