b68f5d6a5f2f115494a9be0e0e66149053fb1570
Arman_Zanganeh <armanzanganeh7@gmail.com>
2026-06-01 22:02:55 +0200

    chore: pure baseline submission for environment validation

--- submission/my_policy.py
+++ submission/my_policy.py
@@ -1,25 +1,7 @@
 import sys
 import submission
-
-
 sys.modules['flatland_baselines'] = submission
 
-from submission.deadlock_avoidance_heuristic.policy.deadlock_avoidance_policy import DeadLockAvoidancePolicy
-from flatland.envs.rail_env import RailEnvActions  # ایمپورت اصلاح شده
+from submission.deadlock_avoidance_heuristic.policy.deadlock_avoidance_policy import DeadlockAvoidanceHeuristics
 
-class MyPolicy(DeadLockAvoidancePolicy):
-    def __init__(self):
-        super().__init__(
-            use_alternative_at_first_intermediate_and_then_always_first_strategy=2,
-            drop_next_threshold=3,
-            k_shortest_path_cutoff=15,
-        )
-
-    def act_many(self, handles, observations, **kwargs):
-        try:
-           
-            return super().act_many(handles, observations, **kwargs)
-        except Exception as e:
-            # در صورت بن‌بستِ مطلق یا باگ در نقشه، زنده بمان و حرکت کن!
-            # print(f"Fallback triggered: {e}") # می‌توانید برای دیباگ باز بگذارید
-            return {a: RailEnvActions.MOVE_FORWARD for a in handles}
+MyPolicy = DeadlockAvoidanceHeuristics
